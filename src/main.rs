f1638e1c8878d277b1d350ca5e412336f959d544
Raul Catalinas <105791463+RaulCatalinas@users.noreply.github.com>
2024-12-14 01:39:30 +0100

    chore: Placeholder removed

--- src/main.rs
+++ src/main.rs
@@ -8,28 +8,5 @@ pub mod types;
 pub mod utils;
 
 fn main() {
-    utils::write_message(utils::WriteMessageProps {
-        message_type: enums::MessageType::Success,
-        message: "StylelintBC started",
-    });
-
-    utils::write_message(utils::WriteMessageProps {
-        message_type: enums::MessageType::Error,
-        message: "StylelintBC stopped",
-    });
-
-    utils::write_message(utils::WriteMessageProps {
-        message_type: enums::MessageType::Info,
-        message: "StylelintBC info",
-    });
-
-    utils::write_message(utils::WriteMessageProps {
-        message_type: enums::MessageType::Warning,
-        message: "StylelintBC warning",
-    });
-
-    utils::write_message(utils::WriteMessageProps {
-        message_type: enums::MessageType::Config,
-        message: "StylelintBC config",
-    });
+    config::configure_options();
 }
